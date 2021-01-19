cb29c0e8715545e39d39a6b7cba3aba94b172919
Atri Hegde <iamatrihegde@gmail.com>
2021-01-19 14:47:00 +0000

    class init

---
+++ flappyBird.py
@@ -0,0 +1,37 @@
+import pygame
+import random
+import os
+import neat
+import time
+
+"""
+after learning the basics of pygame , i wanted to create something cool, so I'm following this tutorial 
+on youtube hoping to learn how to implement what i have learnt
+https://www.youtube.com/watch?v=MMxFDaIOHsE&list=PLzMcBGfZo4-lwGZWXz5Qgta_YNX3_vLS2
+"""
+
+WIN_WIDTH = 600
+WIN_HEIGHT = 800
+
+
+BIRD_IMGS = [pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird1.png"))), pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird2.png"))), pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird3.png")))]
+PIPE = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "pipe.png")))
+BASE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "base.png")))
+BG_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bg.png")))
+
+
+class Bird:
+    IMGS = BIRD_IMGS
+    MAX_ROTATION =25
+    ROT_VEL = 20
+    ANIMATION_TIME = 5
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.tilt = 0
+        self.tick_count = 0
+        self.vel = 0
+        self.height = self.y
+        self.img_count = 0
+        self.img = self.IMGS[0]
